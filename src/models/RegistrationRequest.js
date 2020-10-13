9e1ece099e605b991a4ce6e28a5447f36fa1cf3a
Dylan Johnson <mdjohnson2@ualr.edu>
2020-10-13 11:39:56 -0500

    Added trim to email field and timestamps on model

--- src/models/RegistrationRequest.js
+++ src/models/RegistrationRequest.js
@@ -1,28 +1,32 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const RegistrationRequestSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+const RegistrationRequestSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    accessLevel: {
+      type: String,
+      enum: ["instructor", "admin"],
+      required: true,
+    },
+    requestStatus: {
+      type: String,
+      enum: ["pending", "approved", "denied"],
+      default: "pending",
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  accessLevel: {
-    type: String,
-    enum: ["instructor", "admin"],
-    required: true,
-  },
-  requestStatus: {
-    type: String,
-    enum: ["pending", "approved", "denied"],
-    default: "pending",
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 /**
   For some reason using const RegistrationRequest = mongoose.model("RegistrationRequest", RegistrationRequestSchema);
