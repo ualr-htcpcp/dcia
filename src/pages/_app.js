eccc0f2867ba95353c555adf4e13064bc34fb7e4
Justin Cypret <jcypret@gmail.com>
2020-10-15 23:22:02 -0500

    Swap out Tailwind for Bootstrap-React

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import "../../styles/tailwind.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  future: {
-    purgeLayersByDefault: true,
-    removeDeprecatedGapUtilities: true,
-  },
-};
