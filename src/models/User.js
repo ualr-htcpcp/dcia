9d2cf2a81a860c10cddc0321c218570311d42668
Dylan Johnson <mdjohnson2@ualr.edu>
2020-10-30 16:24:33 -0500

    added hook to send reset email and hook to clear token/expiration after reset

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
-import { checkPassword } from "../utils/auth";
+import { checkPassword, hashPassword } from "../utils/auth";
+import { sendPasswordResetEmail } from "../utils/email";
 
 const UserSchema = new Schema(
   {
@@ -27,9 +28,38 @@ const UserSchema = new Schema(
   { timestamps: true }
 );
 
+UserSchema.methods.checkResetToken = function (token) {
+  const now = new Date();
+  if (now > this.passwordReset.expiration) {
+    return false;
+  }
+  return this.passwordReset.token === token;
+};
 UserSchema.methods.checkPassword = function (password) {
   const passwordHash = this.password;
   return checkPassword(password, passwordHash);
 };
+
+// Reset password, clear passwordReset token/expiry
+UserSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) return next();
+    const hashed = await hashPassword(this.password);
+    this.password = hashed;
+    this.passwordReset.token = null;
+    this.passwordReset.expiration = null;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// Password reset requested
+UserSchema.post("save", async function (doc, next) {
+  if (doc.passwordReset.token && doc.passwordReset.expiration) {
+    await sendPasswordResetEmail(doc.email, doc.passwordReset.token);
+  }
+  next();
+});
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
