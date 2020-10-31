9699a397f5a709c9be83cc41ef861ca96fbc268c
Scott White <white.scottbrandon@gmail.com>
2020-10-31 14:01:14 -0500

    modified:   src/models/Assessment.js 	modified:   src/models/Instructor.js

--- src/models/Assessment.js
+++ src/models/Assessment.js
@@ -1,21 +1,25 @@
 import mongoose from "mongoose";
 
-const assessmentSchema = new mongoose.Schema({
+const assessmentSchema = new mongoose.Schema(
+  {
     score: {
       type: Number,
       required: true,
-      enum: [0, 1, 2, 3, 4]
+      enum: [0, 1, 2, 3, 4],
     },
     student: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: 'Student'
+      ref: "Student",
     },
     studentWorkProject: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: 'StudentWorkProject'
-    }
-  }, {timestamps: true})
+      ref: "StudentWorkProject",
+    },
+  },
+  { timestamps: true }
+);
 
-  export default mongoose.models.Assessment || mongoose.model("Assessment", AssessmentSchema);
+export default mongoose.models.Assessment ||
+  mongoose.model("Assessment", assessmentSchema);
--- src/models/Instructor.js
+++ src/models/Instructor.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
 
-const instructorSchema = new mongoose.Schema({
-    name :{
-        first: String,
-        last: String
+const instructorSchema = new mongoose.Schema(
+  {
+    name: {
+      first: String,
+      last: String,
     },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
-    }
-}, {timestamps: true})
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.models.Instructor ||  mongoose.model('Instructor', instructorSchema)
+export default mongoose.models.Instructor ||
+  mongoose.model("Instructor", instructorSchema);
