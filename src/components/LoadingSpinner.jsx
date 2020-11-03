d37ce64a4ff588f75a540e3d5f7bcf0d895b7a67
Dylan Johnson <mdjohnson2@ualr.edu>
2020-11-03 13:49:45 -0600

    removed fragment wrapper from single node component

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -2,10 +2,8 @@ import { Spinner } from "react-bootstrap";
 
 export default function LoadingSpinner() {
   return (
-    <>
-      <Spinner animation="border" variant="primary">
-        <span className="sr-only">Loading...</span>
-      </Spinner>
-    </>
+    <Spinner animation="border" variant="primary">
+      <span className="sr-only">Loading...</span>
+    </Spinner>
   );
 }
