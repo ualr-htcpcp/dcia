8eb101000dbb2c2c66807a9cc2f6f8960fb88dd0
Dylan Johnson <mdjohnson2@ualr.edu>
2020-11-05 14:25:24 -0600

    fixed typo around adding mongoose debug mode

--- src/middleware/database.js
+++ src/middleware/database.js
@@ -7,7 +7,7 @@ const client = mongoose.connect(process.env.MONGO_URL, {
   useFindAndModify: false,
 });
 if (process.env.NODE_ENV !== "production") {
-  client.set("debug", true);
+  mongoose.set("debug", true);
 }
 
 async function database(req, res, next) {
