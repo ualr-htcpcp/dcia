6762310901aa63657d356c15a6c5a2e19998d225
Scott White <white.scottbrandon@gmail.com>
2020-11-04 18:44:33 -0600

    modified:   seeders/studentOutcomes.seeder.js

--- seeders/studentOutcomes.seeder.js
+++ seeders/studentOutcomes.seeder.js
@@ -4,45 +4,50 @@ import StudentOutcome from "../src/models/StudentOutcome";
 const data = [
   {
     studentOutcomeNumber: 1,
-    definition: "Analyze a complex computing problem and to apply principles of computing and other relevant disciplines to identify solutions.",
+    definition:
+      "Analyze a complex computing problem and to apply principles of computing and other relevant disciplines to identify solutions.",
   },
   {
     studentOutcomeNumber: 2,
-    definition: "Design, implement, and evaluate a computing-based solution to meet a given set of computing requirements in the context of the program’s discipline."
+    definition:
+      "Design, implement, and evaluate a computing-based solution to meet a given set of computing requirements in the context of the program’s discipline.",
   },
   {
     studentOutcomeNumber: 3,
-    definition: "Communicate effectively in a variety of professional contexts."
+    definition:
+      "Communicate effectively in a variety of professional contexts.",
   },
   {
     studentOutcomeNumber: 4,
-    definition: "Recognize professional responsibilities and make informed judgments in computing practice based on legal and ethical principles."
+    definition:
+      "Recognize professional responsibilities and make informed judgments in computing practice based on legal and ethical principles.",
   },
   {
     studentOutcomeNumber: 5,
-    definition: "Function effectively as a member or leader of a team engaged in activities appropriate to the program’s discipline."
+    definition:
+      "Function effectively as a member or leader of a team engaged in activities appropriate to the program’s discipline.",
   },
   {
     studentOutcomeNumber: 6,
-    definition: "Apply computer science theory and software development fundamentals to produce computing-based solutions."
+    definition:
+      "Apply computer science theory and software development fundamentals to produce computing-based solutions.",
   },
 ];
 
 class StudentOutcomesSeeder extends Seeder {
-    async shouldRun() {
-      return StudentOutcome.countDocuments()
-        .exec()
-        .then((count) => count === 0);
-    }
-  
-    async run() {
-      return Promise.all(
-        data.map(async (studentOutcome) => {
-          return StudentOutcome.create(studentOutcome);
-        })
-      );
-    }
+  async shouldRun() {
+    return StudentOutcome.countDocuments()
+      .exec()
+      .then((count) => count === 0);
   }
-  
-  export default StudentOutcomesSeeder;
-  
+
+  async run() {
+    return Promise.all(
+      data.map(async (studentOutcome) => {
+        return StudentOutcome.create(studentOutcome);
+      })
+    );
+  }
+}
+
+export default StudentOutcomesSeeder;
