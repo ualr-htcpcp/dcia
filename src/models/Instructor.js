046d5dde54268103a21a1d57b2a7238972fb19bf
Justin Cypret <jcypret@gmail.com>
2020-11-11 22:28:03 -0600

    Update index to ignore case and whitespace and ensure letters-only

--- src/models/Instructor.js
+++ src/models/Instructor.js
@@ -1,16 +1,26 @@
 import mongoose from "mongoose";
 
+const nameOptions = {
+  type: String,
+  required: true,
+  trim: true,
+  match: /^[a-zA-Z\s]+$/,
+};
+
 const instructorSchema = new mongoose.Schema(
   {
     name: {
-      first: String,
-      last: String,
+      first: nameOptions,
+      last: nameOptions,
     },
   },
   { timestamps: true }
 );
 
-instructorSchema.index({ name: 1 }, { unique: true });
+instructorSchema.index(
+  { name: 1 },
+  { unique: true, collation: { locale: "en", strength: 2 } }
+);
 
 export default mongoose.models.Instructor ||
   mongoose.model("Instructor", instructorSchema);
