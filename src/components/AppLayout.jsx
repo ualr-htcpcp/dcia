0951643e80da7544ec806bff8effc5b9026abaaf
Justin Cypret <jcypret@gmail.com>
2020-11-13 19:04:28 -0600

    Show progress bar while changing between pages

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,29 +1,26 @@
+import MenuBar from "components/MenuBar.jsx";
+import Navigation from "components/Navigation.jsx";
+import { useSession } from "next-auth/client";
 import React from "react";
-import { getSession } from "next-auth/client";
 import { Col, Row } from "react-bootstrap";
-import MenuBar from "../components/MenuBar.jsx";
-import Navigation from "../components/Navigation.jsx";
 
-export default class AppLayout extends React.Component {
-  async componentDidMount() {
-    const session = await getSession();
-    this.setState({ session });
-  }
+export default function AppLayout({ children }) {
+  const [session] = useSession();
 
-  render() {
-    return (
-      <div className="vh-100 d-flex flex-column">
-        <MenuBar />
+  if (!session) return null;
 
-        <Row noGutters className="flex-fill">
-          <Col md={3} xl={2} className="bg-white p-3 border-right">
-            <Navigation session={this.state?.session} />
-          </Col>
-          <Col as="main" md={9} xl={10} className="px-3 py-4">
-            {this.props.children}
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+  return (
+    <div className="vh-100 d-flex flex-column">
+      <MenuBar />
+
+      <Row noGutters className="flex-fill">
+        <Col md={3} xl={2} className="bg-white p-3 border-right">
+          <Navigation session={session} />
+        </Col>
+        <Col as="main" md={9} xl={10} className="px-3 py-4">
+          {children}
+        </Col>
+      </Row>
+    </div>
+  );
 }
