098332515761472113cff68eefdec208f9ba21d7
Justin Cypret <jcypret@gmail.com>
2020-11-13 22:00:25 -0600

    Move useSession into navigation component

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,21 +1,16 @@
 import MenuBar from "components/MenuBar.jsx";
 import Navigation from "components/Navigation.jsx";
-import { useSession } from "next-auth/client";
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 
 export default function AppLayout({ children }) {
-  const [session] = useSession();
-
-  if (!session) return null;
-
   return (
     <div className="vh-100 d-flex flex-column">
       <MenuBar />
 
       <Row noGutters className="flex-fill">
         <Col md={3} xl={2} className="bg-white p-3 border-right">
-          <Navigation session={session} />
+          <Navigation />
         </Col>
         <Col as="main" md={9} xl={10} className="px-3 py-4">
           {children}
