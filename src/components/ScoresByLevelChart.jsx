3bc8e6235a63bb39714aea14562b72422469fa93
Dylan Johnson <mdjohnson2@ualr.edu>
2020-11-15 18:21:54 -0600

    Include graph on SO scores by level component

--- src/components/ScoresByLevelChart.jsx
+++ src/components/ScoresByLevelChart.jsx
@@ -1,11 +1,22 @@
 import { Card } from "react-bootstrap";
+import LineGraph from "./LineGraph.jsx";
+import { springData } from "../graphData";
+
+const xLabel = "Course Level";
+const xKey = "level";
+const yLabel = "Score";
 
 export default function ScoresByLevelChart({ className }) {
   return (
     <Card className={className}>
       <Card.Header className="bg-white">SO Scores by LEVEL</Card.Header>
       <Card.Body>
-        <p className="display-1 text-secondary text-center py-5">Line Chart</p>
+        <LineGraph
+          data={springData}
+          xAxisLabel={xLabel}
+          xAxisKey={xKey}
+          yAxisLabel={yLabel}
+        />
       </Card.Body>
     </Card>
   );
