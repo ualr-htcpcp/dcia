17b14071c57b399274de5a1d355765bdb9d2977f
Dylan Johnson <mdjohnson2@ualr.edu>
2020-12-01 10:50:21 -0600

    move ContainerFix to its own file to share with BarGraph

--- src/components/graphs/LineGraph.jsx
+++ src/components/graphs/LineGraph.jsx
@@ -4,33 +4,11 @@ import {
   Legend,
   Line,
   LineChart,
-  ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
 } from "recharts";
-
-// Fix for recharts ResponsiveContainer not resizing correctly
-// see: https://github.com/recharts/recharts/issues/172#issuecomment-523362671
-function ContainerFix({ children }) {
-  return (
-    <div
-      style={{ position: "relative", width: "100%", paddingBottom: "250px" }}
-    >
-      <div
-        style={{
-          position: "absolute",
-          left: 0,
-          right: 0,
-          bottom: 0,
-          top: 0,
-        }}
-      >
-        <ResponsiveContainer>{children}</ResponsiveContainer>
-      </div>
-    </div>
-  );
-}
+import ContainerFix from "./ContainerFix.jsx";
 
 const graphWidth = 500;
 const graphHeight = 500;
