f4f7bda0bc5aaad5518ae0ebe7f32265c13d6700
Dylan Johnson <mdjohnson2@ualr.edu>
2020-12-03 14:57:14 -0600

    re-package queries in JS exports

---
+++ queries/ScoreCountsByCourseInstance.js
@@ -0,0 +1,110 @@
+// Returns a SUM of each SO Score by COURSE INSTANCE
+
+export function ScoreCountsByCourseInstance(courseInstance) {
+  return [
+    {
+      $match: {
+        _id: courseInstance,
+      },
+    },
+    {
+      $unwind: {
+        path: "$student",
+      },
+    },
+    {
+      $unwind: {
+        path: "$studentWorkProject",
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        student: 1,
+        studentWorkProject: 1,
+      },
+    },
+    {
+      $lookup: {
+        from: "assessments",
+        let: {
+          id: "$student",
+          swp: "$studentWorkProject",
+        },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $and: [
+                  {
+                    $eq: ["$student", "$$id"],
+                  },
+                  {
+                    $eq: ["$studentWorkProject", "$$swp"],
+                  },
+                ],
+              },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              score: 1,
+              studentOutcome: 1,
+            },
+          },
+        ],
+        as: "soAndScore",
+      },
+    },
+    {
+      $unwind: {
+        path: "$soAndScore",
+      },
+    },
+    {
+      $unwind: {
+        path: "$soAndScore.studentOutcome",
+      },
+    },
+    {
+      $lookup: {
+        from: "studentoutcomes",
+        let: {
+          so: "$soAndScore.studentOutcome",
+        },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $eq: ["$_id", "$$so"],
+              },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              studentOutcomeNumber: 1,
+            },
+          },
+        ],
+        as: "so",
+      },
+    },
+    {
+      $unwind: {
+        path: "$so",
+      },
+    },
+    {
+      $group: {
+        _id: {
+          studentOutcome: "$soAndScore.score",
+        },
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+  ];
+}
