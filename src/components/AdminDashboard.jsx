dec08a2cb4f0137e3945268d1a8ad4b964d28e53
Dylan Johnson <mdjohnson2@ualr.edu>
2020-12-05 15:37:34 -0600

    remove unused imports, add SWR error handling

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,24 +1,35 @@
 import { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Alert, Col, Row } from "react-bootstrap";
 import useSWR from "swr";
 import { fetcher } from "utils/fetch.js";
 import ScoresByLevelChart from "./graphs/ScoresByLevelChart.jsx";
-import LoadingSpinner from "./LoadingSpinner.jsx";
 import ScoresByCourseCard from "./ScoresByCourseCard.jsx";
 import ScoresByInstructorCard from "./ScoresByInstructorCard.jsx";
-import ScoreSummaryCard from "./ScoreSummaryCard.jsx";
 import TermSelector from "./TermSelector.jsx";
 
 const TERMS_PATH = "/api/analysis/terms";
+const defaultError = "There was an error. Try refreshing the page.";
 
 export default function AdminDashboard() {
-  const { data: terms, error } = useSWR(TERMS_PATH, fetcher); //TODO: Error case, maybe refresh page button?
+  const { data: terms, error } = useSWR(TERMS_PATH, fetcher);
   const [selectedTerm, setSelectedTerm] = useState(null);
 
   return (
     <>
       {!terms ? (
-        "Loading..."
+        <Row className="mt-4">
+          <Col>
+            {error ? (
+              <Alert variant="danger">
+                {defaultError}
+                <br />
+                <code>{error.message}</code>
+              </Alert>
+            ) : (
+              "Loading..."
+            )}
+          </Col>
+        </Row>
       ) : (
         <TermSelector
           className="mt-4"
