ce3cc1acaaecce6d39ec9deda4bc3fae468444e3
Dylan Johnson <mdjohnson2@ualr.edu>
2020-12-05 19:29:02 -0600

    use live term data at API

--- src/pages/api/analysis/terms.js
+++ src/pages/api/analysis/terms.js
@@ -1,13 +1,25 @@
 import middleware from "middleware";
 import nextConnect from "next-connect";
-import { termData } from "fakeDashboardData";
+import Semester from "models/Semester";
 
 const handler = nextConnect();
 handler.use(middleware);
 
+function sortTerms(a, b) {
+  const termOrder = ["spring", "summer", "fall"];
+  if (a.year === b.year) {
+    return termOrder.indexOf(a.term) - termOrder.indexOf(b.term);
+  }
+  return a.year - b.year;
+}
+
 // Fetch terms to populate term selector component
 handler.get(async (req, res) => {
-  res.json(termData);
+  const terms = await Semester.find({})
+    .select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 })
+    .lean();
+  terms.sort(sortTerms);
+  res.json(terms);
 });
 
 export default handler;
