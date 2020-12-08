168c22fd784cf82547ed2642775604527ad4cb77
Dylan Johnson <mdjohnson2@ualr.edu>
2020-12-07 22:27:42 -0600

    remove limit on return data for scores by instructor/course cards

--- src/components/ScoresByCourseCard.jsx
+++ src/components/ScoresByCourseCard.jsx
@@ -4,13 +4,13 @@ import BlankSummaryList from "./BlankSummaryList.jsx";
 import ScoreSummaryCard from "./ScoreSummaryCard.jsx";
 
 const COURSE_PATH = "/api/analysis/scores_by/course";
-const previewAmount = 5;
+const previewAmount = 5; // add previewAmount to fetch to limit return data
 const viewAllPath = "/courses";
 const name = "SO Scores by COURSE";
 
 export default function ScoresByCourseCard({ term }) {
   const { data: items, error } = useSWR(
-    term ? [COURSE_PATH, term.year, term.term, previewAmount] : null,
+    term ? [COURSE_PATH, term.year, term.term] : null,
     fetchWithYearAndTerm
   );
   return (
--- src/components/ScoresByInstructorCard.jsx
+++ src/components/ScoresByInstructorCard.jsx
@@ -4,13 +4,13 @@ import BlankSummaryList from "./BlankSummaryList.jsx";
 import ScoreSummaryCard from "./ScoreSummaryCard.jsx";
 
 const INSTRUCTOR_PATH = "/api/analysis/scores_by/instructor";
-const previewAmount = 5;
+const previewAmount = 5; // add previewAmount to fetch to limit return data
 const viewAllPath = "/instructors";
 const name = "SO Scores by INSTRUCTOR";
 
 export default function ScoresByInstructorCard({ term }) {
   const { data: items, error } = useSWR(
-    term ? [INSTRUCTOR_PATH, term.year, term.term, previewAmount] : null,
+    term ? [INSTRUCTOR_PATH, term.year, term.term] : null,
     fetchWithYearAndTerm
   );
   return (
