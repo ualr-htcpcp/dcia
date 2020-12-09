96e058d7ad2a178e73f2e8bd25a9dd95f8bea859
Dylan Johnson <mdjohnson2@ualr.edu>
2020-12-08 19:10:33 -0600

    remove unneeded fetch function

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -11,24 +11,6 @@ export async function fetchWithYearAndTerm(baseURL, year, term, amount = null) {
   return response.json();
 }
 
-export async function fetchWithInstructor(
-  baseURL,
-  type,
-  email = null,
-  first = null,
-  last = null
-) {
-  let url;
-  if (type === "ALL") {
-    url = `${baseURL}?type=ALL`;
-  }
-
-  //TODO: handle name case (interaction from instructor dropdown)
-  //TODO: handle email case (scope to logged in instructor)
-  const response = await fetch(url);
-  return response.json();
-}
-
 export async function fetchWithCourseId(baseURL, courseId) {
   const url = `${baseURL}?id=${courseId}`;
 
