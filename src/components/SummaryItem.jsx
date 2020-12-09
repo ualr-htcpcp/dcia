3a3eda3a901de440269485ff14c5b964d446f06f
Justin Cypret <jcypret@gmail.com>
2020-12-09 00:17:23 -0600

    Force course and instructor scores to two decimal places

--- src/components/SummaryItem.jsx
+++ src/components/SummaryItem.jsx
@@ -1,18 +1,13 @@
 import { ListGroup } from "react-bootstrap";
 import { getPillStyle } from "utils/analysis";
 
-function trimScore(score) {
-  const trimmed = new Number(score).toPrecision(2);
-  return trimmed;
-}
-
 export default function SummaryItem({ name, score }) {
   const pillStyle = getPillStyle(score);
   return (
     <ListGroup.Item className="d-flex justify-content-between align-items-center">
       {name ? name : "Loading"}
       {score ? (
-        <span className={pillStyle}>{trimScore(score)}</span>
+        <span className={pillStyle}>{score.toFixed(2)}</span>
       ) : (
         <span className="badge badge-pill badge-secondary"> {"X.x"}</span>
       )}
